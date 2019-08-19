2ca306e42c9036520ade30eb76dd9ea8a36418d1
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-19 02:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob'
 
-function upperCaseCustomerName()
+function upperCaseCustomerName(name)
 {
-  return customerName.toUpperCase();
+  return name.toUpperCase();
 }
